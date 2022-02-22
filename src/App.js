808ac8733e133cbf205bed0deed0c0f3c145e74d
Le Minh Khoi <lmkhoi2k@gmail.com>
2022-02-22 21:02:13 +0700

    upload colorbox

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import TodoFeatures from './features/Todo';
+import ColorBox from './components/ColorBox';
 
 function App() {
   return (
     <div className="App">
-      <TodoFeatures />
+      <ColorBox />
     </div>
   );
 }
